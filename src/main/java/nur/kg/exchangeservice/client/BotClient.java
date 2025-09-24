a9adb37043b13277e0e57b43a290522b619ba4bf
nurdas <nurdas00@gmail.com>
2025-09-24 23:39:40 +0600

    refactor for websocket

--- src/main/java/nur/kg/exchangeservice/client/BotClient.java
+++ src/main/java/nur/kg/exchangeservice/client/BotClient.java
@@ -2,10 +2,14 @@ package nur.kg.exchangeservice.client;
 
 import nur.kg.exchangeservice.domain.Ticker;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
 public class BotClient {
+
+    private final WebClient webClient = WebClient.builder().build();
     public void sendData(Ticker ticker) {
 
+        webClient.post();
     }
 }
