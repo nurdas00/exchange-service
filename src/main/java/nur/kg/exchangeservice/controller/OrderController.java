3fa217e7e14e0a791dea55479a803de62656bb99
nurdas <nurdas00@gmail.com>
2025-10-17 20:49:53 +0600

    log request

--- src/main/java/nur/kg/exchangeservice/controller/OrderController.java
+++ src/main/java/nur/kg/exchangeservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package nur.kg.exchangeservice.controller;
 
 import com.bybit.api.client.domain.trade.response.OrderResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import nur.kg.domain.request.OrderRequest;
 import nur.kg.exchangeservice.service.OrderService;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+@Log4j2
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/orders")
@@ -19,6 +21,7 @@ public class OrderController {
 
     @PostMapping
     public Mono<OrderResponse> placeOrder(@RequestBody Mono<OrderRequest> request) {
+        log.info("Receive request: {}",  request);
         return request.flatMap(orderService::placeOrder);
     }
 }
